db9d18e697dc2f817946419a16196433adbd15cf
Aditya Chawla <138515367+adityachawla25@users.noreply.github.com>
2023-09-21 20:18:49 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -2,9 +2,9 @@ import java.util.*;
 public class calculator {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first number : ");
+        System.out.println("Enter the first number: ");
         int a = sc.nextInt();
-        System.out.println("Enter the second number : ");
+        System.out.println("Enter the second number: ");
         int b = sc.nextInt();
         System.out.println("Enter operator :");
         char operator=sc.next().charAt(0);
